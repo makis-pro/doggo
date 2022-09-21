b215fbecc8a3432d8af803872f33d2b79f6e9bb6
Sandra Nwakeze <sandramakis.dev@gmail.com>
2022-09-21 14:53:26 +0100

    Menu and nav dark theme

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// MENU TOGGLE BETWEEN THE OPEN
+// MENU TOGGLE BETWEEN THE OPEN STATES
 
 const menu = document.querySelector(".menu");
 const nav = document.querySelector("nav");
@@ -9,21 +9,25 @@ menu.addEventListener("click", () => {
 
 // DARK THEME FUNCTION
 
-const body = document.querySelector("body");
 const header = document.querySelector("header");
 const section1 = document.querySelector(".section1");
 const main = document.querySelector("main");
+const form = document.querySelector("form");
 const section2 = document.querySelector(".section2");
 const footer = document.querySelector("footer");
 const toggleIcon = document.querySelector("#dark-theme");
 toggleIcon.addEventListener("click", () => {
-  body.classList.toggle("dark-theme");
   header.classList.toggle("dark-theme");
   header.style.transition = "all .3s";
-  section1.classList.toggle("dark-theme");
-  section1.style.transition = "all .3s";
+  menu.classList.toggle("dark-theme");
+  nav.classList.toggle("dark-theme");
+  nav.style.transition = "all .3s";
   main.classList.toggle("dark-theme");
   main.style.transition = "all .3s";
+  form.classList.toggle = ".dark-theme";
+  form.style.transition = "all .3s";
+  section1.classList.toggle("dark-theme");
+  section1.style.transition = "all .3s";
   section2.classList.toggle("dark-theme");
   section2.style.transition = "all .3s";
   footer.classList.toggle("dark-theme");
@@ -34,10 +38,8 @@ toggleIcon.addEventListener("click", () => {
 
 function formFunction() {
   const logIn = document.querySelector("#log-in");
-  const signIn = document.querySelector("#sign-in");
   const form1 = document.querySelector("#form1");
   const form2 = document.querySelector("#form2");
-  const getStarted = document.querySelector(".get-started");
   logIn.addEventListener("click", () => {
     if ((form1.style.display = "none") && (form2.style.display = "block")) {
       form1.style.display = "block";
@@ -46,46 +48,27 @@ function formFunction() {
       form1.style.display = "none";
     }
   });
-
-  getStarted.addEventListener("click", () => {
-    if ((form1.style.display = "none")) {
-      form1.style.display = "block";
-    } else {
-      form1.style.display = "none";
-    }
-  });
-
-  signIn.addEventListener("click", () => {
-    if ((form2.style.display = "none") && (form1.style.display = "block")) {
-      form2.style.display = "block";
-      form1.style.display = "none";
-    } else {
-      form2.style.display = "block";
-    }
-  });
 }
 
 formFunction();
 
-const btn = document.querySelector("#btn");
-const dogs = document.querySelector("#dogs");
+// const btn = document.querySelector("#btn");
+// const dogs = document.querySelector("#dogs");
 
-btn.addEventListener("click", () => {
-  if (dogs.classList.contains("hidden")) {
-    dogs.classList.remove("hidden");
-  } else {
-    dogs.classList.add("hidden");
-  }
-});
-
-const getStarted = document.querySelector("#getStarted");
+// btn.addEventListener("click", () => {
+//   if (dogs.classList.contains("hidden")) {
+//     dogs.classList.remove("hidden");
+//   } else {
+//     dogs.classList.add("hidden");
+//   }
+// });
 
 // ========FORM FILLING(not fully active)======
-const eMail = document.querySelector("#eMail");
-eMail.addEventListener("click", () => {
-  if (eMail.contains(".com", ".yahoo", ".outlook")) {
-    alert("The e-mail address is valid");
-  } else {
-    alert("Please input a valid E-mail account");
-  }
-});
+// const eMail = document.querySelector("#eMail");
+// eMail.addEventListener("click", () => {
+//   if (eMail.contains(".com", ".yahoo", ".outlook")) {
+//     alert("The e-mail address is valid");
+//   } else {
+//     alert("Please input a valid E-mail account");
+//   }
+// });
