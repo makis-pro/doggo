576f677b2bf2e92f5971cf6cdf03a6ebc6d385c4
Sandra Nwakeze <sandramakis.dev@gmail.com>
2022-10-05 16:22:26 +0100

    FUCT. buttons(add to cart and delete

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// MENU TOGGLE BETWEEN THE OPEN STATES
+// MENU TOGGLE BETWEEN THE OPEN and CLOSED STATES
 
 const menu = document.querySelector(".menu");
 const nav = document.querySelector("nav");
@@ -19,17 +19,24 @@ const toggleIcon = document.querySelector("#dark-theme");
 toggleIcon.addEventListener("click", () => {
   header.classList.toggle("dark-theme");
   header.style.transition = "all .3s";
+
   menu.classList.toggle("dark-theme");
+
   nav.classList.toggle("dark-theme");
   nav.style.transition = "all .3s";
+
   main.classList.toggle("dark-theme");
   main.style.transition = "all .3s";
-  form.classList.toggle = ".dark-theme";
+
+  form.classList.toggle = "dark-theme";
   form.style.transition = "all .3s";
+
   section1.classList.toggle("dark-theme");
   section1.style.transition = "all .3s";
+
   section2.classList.toggle("dark-theme");
   section2.style.transition = "all .3s";
+
   footer.classList.toggle("dark-theme");
   footer.style.transition = "all .3s";
 });
@@ -52,23 +59,57 @@ function formFunction() {
 
 formFunction();
 
-// const btn = document.querySelector("#btn");
-// const dogs = document.querySelector("#dogs");
+// ADD TO CART VARIABLES
 
-// btn.addEventListener("click", () => {
-//   if (dogs.classList.contains("hidden")) {
-//     dogs.classList.remove("hidden");
-//   } else {
-//     dogs.classList.add("hidden");
-//   }
-// });
+const addToCart = document.querySelectorAll(".add-to-cart");
+const deleteCart = document.querySelectorAll(".fa-trash-can");
+const dogSection = document.querySelectorAll(".dog");
 
-// ========FORM FILLING(not fully active)======
-// const eMail = document.querySelector("#eMail");
-// eMail.addEventListener("click", () => {
-//   if (eMail.contains(".com", ".yahoo", ".outlook")) {
-//     alert("The e-mail address is valid");
-//   } else {
-//     alert("Please input a valid E-mail account");
-//   }
+// deleteCart.forEach((del) => {
+//   del.style.display = "none";
 // });
+// ADD FUNCTION
+
+addToCart.forEach((cart) => {
+  cart.addEventListener("click", (e) => {
+    e.preventDefault();
+    setTimeout(() => {
+      cart.textContent = "Added to Cart";
+      cart.style.backgroundColor = "green";
+      cart.style.transition = "ease-out .3s";
+      cart.style.fontStyle = "italic";
+    }, 500);
+    console.log(cart);
+
+    displayTrashBin();
+  });
+});
+
+function displayTrashBin() {
+  const span = document.querySelectorAll(".dog span");
+  const dog = document.querySelectorAll(".dog");
+
+  const trashIcon = `<i class="fa-solid fa-trash-can"></i>`;
+
+  span.innerHTML += trashIcon;
+  dog.appendChild(span);
+}
+
+// DELETE CART fUNCTION
+
+deleteCart.forEach((trash) => {
+  trash.addEventListener("click", () => {
+    deleteDogSection(); //assigned the delete function to each clicked trash
+  });
+});
+
+//calling the deleteDogSelection function
+function deleteDogSection() {
+  dogSection.forEach((dog) => {
+    dog.addEventListener("click", (e) => {
+      e.preventDefault();
+      dog.style.display = "none";
+    });
+  });
+  console.log("success");
+}
